9e1ae73a7ffe9deab7286cce09651e6c7b8eb3b5
wolfgangserque <wolfgangserque@gmail.com>
2024-05-31 00:53:06 +0700

    first commit

---
+++ perkalian.cpp
@@ -0,0 +1,26 @@
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // Declare Variables
+
+    int num1, num2, hasil;
+
+    std::cout << "Masukan bilangan pertama:";
+    std::cin >> num1;
+    std::cout << "Masukan bilangan kedua:";
+    std::cin >> num2;
+
+    // Lakukan perkalian
+    hasil = num1 * num2 ;
+
+    std::cout<< "Hasil adalah :"<< hasil << std::endl;
+
+    return 0;
+
+
+}
